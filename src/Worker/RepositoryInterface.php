cc31568938d27fd5e02c029ec9cf9b5279d143cf
Karol Dąbrowski <20278897+karoldabro@users.noreply.github.com>
2022-01-29 13:03:34 +0100

    Add worker interfaces

---
+++ src/Worker/RepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kdabrow\CryptoWorkerContract\Worker;
+
+use Illuminate\Collections\Collection;
+use Kdabrow\CryptoWorkerContract\DataObjects\Kline;
+use Carbon\CarbonInterface;
+
+interface RepositoryInterface
+{
+    /**
+     * Check if source has newest klines from exchange
+     *
+     * @return bool
+     */
+    public function isSynced(CarbonInterface $since): bool;
+
+    /**
+     * Update klines in source
+     *
+     * @return bool
+     */
+    public function update(): bool;
+
+    /**
+     * Return klines from source
+     * 
+     * @param CarbonInterface $since
+     * @param CarbonInterface $until
+     * @return Collection<int, Kline>
+     */
+    public function get(CarbonInterface $since, CarbonInterface $until): Collection;
+}
---
+++ src/Worker/WorkerDataInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kdabrow\CryptoWorkerContract\Worker;
+
+use Carbon\CarbonInterval;
+use Kdabrow\CryptoWorkerContract\Strategy\Enums\Interval;
+
+interface WorkerDataInterface
+{
+    public function getStrategyId(): string;
+
+    public function getExchangeId(): string;
+
+    public function getPair(): string;
+
+    public function getKlineInterval(): Interval|string;
+
+    public function getKlineQuantity(): int;
+
+    public function getRefreshInterval(): CarbonInterval;
+}
